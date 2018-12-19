f067b255262b0974c60ba8ff19c21bd2b1230d31
micahlagrange <micah@lagrangesystems.com>
2018-12-19 11:36:07 -0700

    remove body

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
+// Implement a form login
 func main() {
 	var (
 		endpoint, username, password, getCookie string
@@ -16,35 +16,34 @@ func main() {
 
 	endpoint, username, password, getCookie = os.Args[1], os.Args[2], os.Args[3], os.Args[4]
 
-	fmt.Printf("Logging into %s with user/pass %s/%s and retrieving header %s\n", endpoint, username, password, getCookie)
-	_, cookie := basicAuth(endpoint, username, password, getCookie)
+	fmt.Printf("Logging into %s with user/pass %s/%s and retrieving header %s\n",
+		endpoint, username, password, getCookie)
+
+	cookie := basicAuth(endpoint, username, password, getCookie)
+
 	fmt.Printf("got cookie: %s\n", cookie)
 }
 
-func basicAuth(endpoint string, username string, password string, getCookie string) (string, string) {
+// Do basic auth, return a cookie value by name
+func basicAuth(endpoint string, username string, password string, getCookie string) string {
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", endpoint, strings.NewReader(fmt.Sprintf("email=%s&password=%s", username, password)))
+	// Add headers needed to post a form
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	req.Header.Add("X-Requested-With", "XMLHttpRequest")
 	req.Header.Add("Accept", "application/json")
-
-	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Add("Accept-Language", "en-US,en;q=0.9")
 
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	bodyText, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	body := string(bodyText)
 
 	cookie := cookieByName(resp, getCookie)
-	return body, cookie
+
+	return cookie
 }
 
+// Take a string and return the value for the cookie whos name == string
 func cookieByName(resp *http.Response, name string) string {
 	for _, c := range resp.Cookies() {
 		if c.Name == name {
